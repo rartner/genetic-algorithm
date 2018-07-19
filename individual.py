f9101f4d30c2299c4f42d09be29715f45ff6dba4
rartner <ramon.artner@gmail.com>
2018-07-19 19:32:06 -0300

    creating new population using individual class

--- individual.py
+++ individual.py
@@ -1,19 +1,4 @@
-from individual_bin import Individual_Bin
-from individual_int import Individual_Int
-from individual_real import Individual_Real
-from individual_perm import Individual_Perm
-
 class Individual():
 
-    ''' Return the object according to the encoding '''
-    def __new__(cls, size, encoding, min_bound, max_bound):
-        if encoding == 'BIN':
-            return Individual_Bin(size)
-        elif encoding == 'INT':
-            return Individual_Int(size, min_bound, max_bound)
-        elif encoding == 'REAL':
-            return Individual_Real(size, min_bound, max_bound)
-        elif encoding == 'INT-PERM':
-            return Individual_Perm(size)
-        else:
-            raise Exception('Invalid encoding')
+    def __init__(self, chromosome):
+        self.chromosome = chromosome
--- init.py
+++ init.py
@@ -1,17 +1,20 @@
-import numpy as np 
+import numpy as np
+from individual import Individual
 
 
 def bin(chromo_size, lower_bound=0, upper_bound=2):
-  return np.random.randint(2, size=chromo_size)
+    return Individual(np.random.randint(2, size=chromo_size))
 
 
 def int(chromo_size, lower_bound=0, upper_bound=10):
-  return np.random.randint(lower_bound, upper_bound, size=100)
+    return Individual(np.random.randint(lower_bound, upper_bound, size=100))
 
 
 def int_perm(chromo_size, lower_bound=0, upper_bound=10):
-  return np.random.permutation(chromo_size)
+    return Individual(np.random.permutation(chromo_size))
 
 
 def real(chromo_size, lower_bound=0, upper_bound=10):
-  return np.random.uniform(lower_bound, upper_bound, size=chromo_size)
+    return Individual(
+        np.random.uniform(lower_bound, upper_bound, size=chromo_size)
+    )
