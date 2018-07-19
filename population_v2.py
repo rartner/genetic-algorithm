1182d0aee8639f2b0182719133063bc543d6d5bc
rartner <ramon.artner@gmail.com>
2018-07-19 19:12:48 -0300

    adding crossover and mutation to the evolutionary process

--- population_v2.py
+++ population_v2.py
@@ -17,9 +17,9 @@ class Population:
         self.description = problem["description"]
         self.encoding = problem["encoding"]
         self.eval_fitness = problem["fitness"]
-        self.selection = problem["selection"]
-        self.crossover = problem["crossover"]
-        self.mutation = problem["mutation"]
+        self.select = problem["selection"]
+        self.mate = problem["crossover"]
+        self.mutate = problem["mutation"]
 
         self.individuals = problem["init_population"](
             chromo_size, lower_bound, upper_bound
@@ -29,6 +29,8 @@ class Population:
         self.chromo_size = chromo_size
         self.crossover_tax = crossover_tax
         self.mutation_tax = mutation_tax
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
 
         self.diversity = []
         self.best_individual = None
@@ -39,9 +41,13 @@ class Population:
         while generation < self.num_generations:
             self.get_diversity()
             self.get_fitness()
-            parents = self.selection(
+            parents = self.select(
               self.individuals, self.pop_size, None
             )
+            next_generation = self.crossover(parents)
+            self.mutation(next_generation)
+            self.individuals = next_generation
+            generation += 1
 
 
     def get_diversity(self):
@@ -55,16 +61,37 @@ class Population:
       max_fitness = 0.0
       for ind in self.individuals:
         ind.fitness = self.eval_fitness(ind)
-        if (ind.fitness > max_fitness):
-            max_fitness = ind.fitness
-            if self.best_individual is None:
-                self.best_individual = deepcopy(ind)
-            if (ind.fitness > self.best_individual.fitness):
-                self.best_individual = deepcopy(ind)
+        if ind.fitness > max_fitness:
+          max_fitness = ind.fitness
+          if self.best_individual is None:
+            self.best_individual = deepcopy(ind)
+          if ind.fitness > self.best_individual.fitness:
+            self.best_individual = deepcopy(ind)
 
 
-    def select(self):
-      pass
+    def crossover(self, parents):
+      next_generation = []
+      for ind in range(0, self.pop_size, 2):
+        ctax = np.random.uniform(0, 1)
+        if ctax < self.crossover_tax:
+          childs = self.mate(parents[ind], parents[ind + 1])
+          next_generation.append(childs[0])
+          next_generation.append(childs[1])
+        else:
+          next_generation.append(parents[ind])
+          next_generation.append(parents[ind + 1])
+      next_generation = sorted(next_generation, key=lambda ind: ind.fitness)
+      next_generation[0] = deepcopy(self.best_individual)
+      return next_generation
+
+
+    def mutation(self, individuals):
+      for ind in individuals:
+        self.mutate(
+          ind, self.mutation_tax, self.lower_bound, self.upper_bound
+        )
+
+
 
 
     
